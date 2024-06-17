e42fc3cfe77448bd68cd7bdb030e71d2854a97a4
Hitesh Tare <hiteshtare@gmail.com>
2024-06-17 17:54:30 +0530

    Added Dynamic data test for Books

--- cypress/e2e/bookstorePreview.cy.ts
+++ cypress/e2e/bookstorePreview.cy.ts
@@ -4,14 +4,17 @@ import { BookstorePreviewPages } from "./pages/bookstorePreview.page";
 // Import custom modules
 const bookstorePreviewPages = new BookstorePreviewPages();
 
+//Import test data for Books
+const testData = require("../fixtures/bookstore_book.json");
+
 describe("Bookstore", () => {
-  describe("magazine:Subscription", () => {
+  describe.skip("magazine:Subscription", () => {
     beforeEach(() => {
       cy.visit("product/yogoda-satsanga-magazine-subscription");
     });
 
     it("Yogoda Satsanga Magazine Subscription should have new link and give 200 OK status", () => {
-      bookstorePreviewPages.validateNewlinkAndStatusCode200(
+      bookstorePreviewPages.validateNewlinkAndStatusCode200ForBooks(
         bookstorePreviewPages.btnLookInside
       );
     });
@@ -32,6 +35,29 @@ describe("Bookstore", () => {
       bookstorePreviewPages.validateNewlinkAndStatusCode200AfterClickScreenshot(
         bookstorePreviewPages.btnLookInside
       );
+    });
+  });
+
+  describe("Dynamic data test for Books", () => {
+    testData.forEach((testCase: any) => {
+      let pattern = /-\d+$/;
+
+      const isMatch = pattern.test(testCase.post_name);
+      console.debug(`Postname: ${testCase.post_name}`);
+
+      //To check if match is true skip else open the page and run tests
+      if (!isMatch) {
+
+        it(`Book with Title:${testCase.post_title} should have new link and give 200 OK status`, () => {
+          cy.visit(`product/${testCase.post_name}`);
+
+          cy.log(`Book with Postname: ${testCase.post_name}`);
+
+          bookstorePreviewPages.validateNewlinkAndStatusCode200ForBooks(
+            bookstorePreviewPages.btnLookInsideForBooks
+          );
+        });
+      }
     });
   });
 });
--- cypress/e2e/pages/bookstorePreview.page.ts
+++ cypress/e2e/pages/bookstorePreview.page.ts
@@ -7,6 +7,8 @@ import { captureSreenshot } from "../util/common.util";
 export class BookstorePreviewPages {
   
   btnLookInside = 'body > div.elementor.elementor-183744.elementor-location-single.post-183040.product.type-product.status-publish.has-post-thumbnail.product_cat-magazine.product_shipping_class-free-shipping.pa_product-language-product-language-bengali.pa_product-language-product-language-english.pa_product-language-product-language-hindi.product-template-type-product-template-type-variable.first.instock.taxable.shipping-taxable.purchasable.product-type-variable.has-default-attributes.product > section.elementor-section.elementor-top-section.elementor-element.elementor-element-aaefea4.elementor-section-boxed.elementor-section-height-default.elementor-section-height-default > div > div.elementor-column.elementor-col-50.elementor-top-column.elementor-element.elementor-element-837ac8b > div > div.elementor-element.elementor-element-377c090.yss_noboxshadow.jedv-enabled--yes.elementor-widget.elementor-widget-text-editor > div > div > div > div > div > div > a';
+  
+  btnLookInsideForBooks = 'a[href$=".pdf"]';
 
   clickEnglishLanguage() {
     cy.get(TEST_CONFIG.bookstorePreview.buttonEnglish).click();
@@ -18,17 +20,7 @@ export class BookstorePreviewPages {
   
   clickBengaliLanguage() {
     cy.get(TEST_CONFIG.bookstorePreview.buttonBengali).click();
-  }
-
-  validateNewlinkAndStatusCode200(selector: string) {
-    captureSreenshot();
-
-    //Check if href contains new link
-    this.checkHrefForNewLink(selector);
-
-    //Check if new link is giving 200 OK response code
-    this.checkStatus200ForNewLink(selector);
-  }
+  }  
 
   validateNewlinkAndStatusCode200AfterClickScreenshot(selector: string) {
     captureSreenshot(1000);
@@ -51,7 +43,7 @@ export class BookstorePreviewPages {
     cy.get(selector).then((link) => {
       cy.request("HEAD", link.prop("href")).its("status").should("eq", 200);
 
-      cy.log(`PDF link: ${link.prop("href")}`);
+      cy.log(`3DIssue link: ${link.prop("href")}`);
 
       //Open this new link and take screenshot
       // if (!TEST_CONFIG.bookstorePreview.skipOpenPDFAndTakeScreenshot){
@@ -67,5 +59,15 @@ export class BookstorePreviewPages {
       .get(".a44")
       .wait(TEST_CONFIG.waitForScreenshot)
       .screenshot();
+  }
+
+  validateNewlinkAndStatusCode200ForBooks(selector: string) {
+    captureSreenshot();
+
+    //Check if href contains new link
+    this.checkHrefForNewLink(selector);
+
+    //Check if new link is giving 200 OK response code
+    this.checkStatus200ForNewLink(selector);
   }
 }
