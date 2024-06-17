688f6e7ef4b7caee9c1a1d222078496779f512d8
Hitesh Tare <hiteshtare@gmail.com>
2024-06-17 11:18:18 +0530

    Implemented Loading of Cypress Settings Per Environment (without allure)

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from "cypress";
 import allureWriter from '@shelex/cypress-allure-plugin/writer';
+import fs from 'fs';
 
 export default defineConfig({
   e2e: {
@@ -8,9 +9,32 @@ export default defineConfig({
     video: true,
     baseUrl: 'https://test.yssofindia.org/',
     setupNodeEvents(on, config) {
+      // // implement node event listeners here
+      // const environmentName = config.env.environmentName || 'staging';
+      // const environmentFilename = `./env/${environmentName}.settings.json`;
+      // console.warn('loading %s', environmentFilename);
+      // const settings = require(environmentFilename);
+      // if (settings.baseUrl) {
+      //   config.baseUrl = settings.baseUrl;
+      // }
+      // if (settings.env) {
+      //   config.env = {
+      //     ...config.env,
+      //     ...settings.env,
+      //   };
+      // }
+      // console.log('loaded settings for environment %s', environmentName)
+    
+      // IMPORTANT: return the updated config object
+      // for Cypress to use it
       allureWriter(on, config);
+      // on('after:run', (results) => {
+      //   const data = `Environment=${environmentName}\nBaseURL=${settings.baseUrl}\n`
+      //   fs.writeFile('allure-results/environment.properties', data, (err) => {
+      //     if (err) throw err;
+      //   });
+      // })
       return config;
-      // implement node event listeners here
     },
   },
 });
