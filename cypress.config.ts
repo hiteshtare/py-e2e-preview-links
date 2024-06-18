447479a98349272749deba056ab4e0f74d865574
Hitesh Tare <hiteshtare@gmail.com>
2024-06-18 10:51:25 +0530

    #Resize the viewport to 1280px x 768px

--- cypress.config.ts
+++ cypress.config.ts
@@ -4,9 +4,15 @@ import fs from 'fs';
 
 export default defineConfig({
   e2e: {
+    //By default this is true
     watchForFileChanges: false,
+    //By default this is 4000
     defaultCommandTimeout: 10000,
+    //By default this is true, Cypress will record a video of the test run when running headlessly.
     video: true,
+    // Resize the viewport to 1280px x 768px
+    viewportWidth: 1280,
+    viewportHeight: 768,
     baseUrl: 'https://test.yssofindia.org/',
     setupNodeEvents(on, config) {
       // // implement node event listeners here
