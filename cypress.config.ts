af131b65140a77f930cef1c4d0f482529710850c
Hitesh Tare <hiteshtare@gmail.com>
2024-06-18 23:20:37 +0530

    Working Cypress Settings per env

--- cypress.config.ts
+++ cypress.config.ts
@@ -15,31 +15,31 @@ export default defineConfig({
     viewportHeight: 768,
     baseUrl: 'https://test.yssofindia.org/',
     setupNodeEvents(on, config) {
-      // // implement node event listeners here
-      // const environmentName = config.env.environmentName || 'staging';
-      // const environmentFilename = `./env/${environmentName}.settings.json`;
-      // console.warn('loading %s', environmentFilename);
-      // const settings = require(environmentFilename);
-      // if (settings.baseUrl) {
-      //   config.baseUrl = settings.baseUrl;
-      // }
-      // if (settings.env) {
-      //   config.env = {
-      //     ...config.env,
-      //     ...settings.env,
-      //   };
-      // }
-      // console.log('loaded settings for environment %s', environmentName)
+      // implement node event listeners here
+      const environmentName = config.env.environmentName || 'staging';
+      const environmentFilename = `./env/${environmentName}.settings.json`;
+      console.warn('loading %s', environmentFilename);
+      const settings = require(environmentFilename);
+      if (settings.baseUrl) {
+        config.baseUrl = settings.baseUrl;
+      }
+      if (settings.env) {
+        config.env = {
+          ...config.env,
+          ...settings.env,
+        };
+      }
+      console.log('loaded settings for environment %s', environmentName)
     
       // IMPORTANT: return the updated config object
       // for Cypress to use it
       allureWriter(on, config);
-      // on('after:run', (results) => {
-      //   const data = `Environment=${environmentName}\nBaseURL=${settings.baseUrl}\n`
-      //   fs.writeFile('allure-results/environment.properties', data, (err) => {
-      //     if (err) throw err;
-      //   });
-      // })
+      on('after:run', (results) => {
+        const data = `Environment=${environmentName}\nBaseURL=${settings.baseUrl}\n`
+        fs.writeFile('allure-results/environment.properties', data, (err) => {
+          if (err) throw err;
+        });
+      })
       return config;
     },
   },
